d1ed56b6f21b9aa02e0d1862ee3de41d4f41e893
HasithaM <jayawardanahm@gmail.com>
2020-01-12 21:32:44 +0530

    Create CharValue.java

---
+++ Characters to Uppercase/CharValue.java
@@ -0,0 +1,16 @@
+public class CharValue {
+
+    private int charValue;
+
+    public CharValue(int charValue) {
+        this.charValue = charValue;
+    }
+
+    public int getCharValue() {
+        return charValue;
+    }
+
+    public void setCharValue(int charValue) {
+        this.charValue = charValue;
+    }
+}
