807ee175513e793d7e2dc334478bb4c9e2e11bc7
HasithaM <jayawardanahm@gmail.com>
2020-01-13 05:51:15 +0530

    Create MethodLocalInnerClass.java

---
+++ Encapsulation/MethodLocalInnerClass/MethodLocalInnerClass.java
@@ -0,0 +1,20 @@
+class MethodLocalInnerClass {
+	
+	void outerMethod() {
+        System.out.println("Inside OuterMethod"); 
+        
+        class Inner {
+            void innerMethod() {
+                System.out.println("Inside InnerMethod"); 
+            }
+        }
+        
+        Inner inner = new Inner();
+        inner.innerMethod();
+    }
+	
+	public static void main(String[] args) {
+		MethodLocalInnerClass methodLocalInnerClass = new MethodLocalInnerClass();
+        methodLocalInnerClass.outerMethod();
+	}
+}
