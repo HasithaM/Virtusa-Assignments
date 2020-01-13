5793e79c89b20c0ad723d56e5a4c2d96ea71cdf9
HasithaM <jayawardanahm@gmail.com>
2020-01-13 05:57:14 +0530

    Update MethodLocalInnerClass.java

--- Encapsulation/MethodLocalInnerClass/MethodLocalInnerClass.java
+++ Encapsulation/MethodLocalInnerClass/MethodLocalInnerClass.java
@@ -1,17 +1,17 @@
 class MethodLocalInnerClass {
 	
 	void outerMethod() {
-        System.out.println("Inside OuterMethod"); 
-        
-        class Inner {
-            void innerMethod() {
-                System.out.println("Inside InnerMethod"); 
-            }
-        }
-        
-        Inner inner = new Inner();
-        inner.innerMethod();
-    }
+		System.out.println("Inside OuterMethod"); 
+
+		class Inner {
+		    void innerMethod() {
+			System.out.println("Inside InnerMethod"); 
+		    }
+		}
+
+		Inner inner = new Inner();
+		inner.innerMethod();
+    	}
 	
 	public static void main(String[] args) {
 		MethodLocalInnerClass methodLocalInnerClass = new MethodLocalInnerClass();
