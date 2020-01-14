2f636b63a2a0f2d1dd47913ef65094c6b1591822
HasithaM <jayawardanahm@gmail.com>
2020-01-14 06:18:12 +0530

    Update StarPattern.java

--- Star Pattern and Multiplication Table/StarPattern.java
+++ Star Pattern and Multiplication Table/StarPattern.java
@@ -2,25 +2,25 @@ public class StarPattern {
 	
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
-            for (int j = 10; j > i; j--) {
-                System.out.print(" ");
-            }
+		    for (int j = 10; j > i; j--) {
+			System.out.print(" ");
+		    }
 
-            for (int k = 0; k < (i * 2) - 1; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+		    for (int k = 0; k < (i * 2) - 1; k++) {
+			System.out.print("*");
+		    }
+		    System.out.println();
+        	}
 
-        for (int i = 10; i > 0; i--) {
-            for (int j = 10; j > i; j--) {
-                System.out.print(" ");
-            }
+		for (int i = 10; i > 0; i--) {
+		    for (int j = 10; j > i; j--) {
+			System.out.print(" ");
+		    }
 
-            for (int k = 0; k < (i * 2) - 1; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+		    for (int k = 0; k < (i * 2) - 1; k++) {
+			System.out.print("*");
+		    }
+		    System.out.println();
+		}
 	}
 }
