f944d9526ee719f4e70c190cddf158a799b14999
HasithaM <jayawardanahm@gmail.com>
2020-01-14 06:15:03 +0530

    Update MultiplicationTable.java

--- Star Pattern and Multiplication Table/MultiplicationTable.java
+++ Star Pattern and Multiplication Table/MultiplicationTable.java
@@ -1,23 +1,23 @@
 public class MultiplicationTable {
 	
 	public static void main(String[] args) {
-		    System.out.print("      ");
+		System.out.print("      ");
         
-        for (int i = 1; i < 10; i++) {
-            System.out.printf("%4d", i);
-        }
-        
-        System.out.println();
-        System.out.println("------------------------------------------");
+		for (int i = 1; i < 10; i++) {
+		    System.out.printf("%4d", i);
+		}
 
-        for (int i = 1; i < 10; i++) {
-            System.out.printf("%4d |", i);
-            
-            for (int j = 1; j < 10; j++) {
-                System.out.printf("%4d", i * j);
-            }
-            
-            System.out.println();
-        }
+		System.out.println();
+		System.out.println("------------------------------------------");
+
+		for (int i = 1; i < 10; i++) {
+		    System.out.printf("%4d |", i);
+
+		    for (int j = 1; j < 10; j++) {
+			System.out.printf("%4d", i * j);
+		    }
+
+		    System.out.println();
+		}
 	}
 }
