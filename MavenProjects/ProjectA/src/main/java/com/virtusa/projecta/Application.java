ff31f24cf4194cd3735cc64a6e99f0a8b8247d96
HasithaM <jayawardanahm@gmail.com>
2020-01-22 23:40:37 +0530

    Add Maven Project Files

---
+++ MavenProjects/ProjectA/src/main/java/com/virtusa/projecta/Application.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.virtusa.projecta;
+
+/**
+ *
+ * @author Hasitha
+ */
+public class Application {
+
+    public void print() {
+        System.out.println("Hi Maven!!!");
+    }
+}
---
+++ MavenProjects/ProjectB/src/main/java/com/virtusa/projectb/MainClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.virtusa.projectb;
+
+import com.virtusa.projecta.Application;
+
+/**
+ *
+ * @author Hasitha
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.print();
+    }
+}
