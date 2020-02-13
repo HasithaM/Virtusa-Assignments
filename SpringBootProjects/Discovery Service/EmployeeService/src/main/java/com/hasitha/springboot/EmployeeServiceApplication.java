869ee71500a38bc853ef3423fc9e1a72cd7c1cda
HasithaM <jayawardanahm@gmail.com>
2020-02-14 00:45:09 +0530

    Updating Discovery Service Project Files

--- SpringBootProjects/Discovery Service/EmployeeService/src/main/java/com/hasitha/springboot/EmployeeServiceApplication.java
+++ SpringBootProjects/Discovery Service/EmployeeService/src/main/java/com/hasitha/springboot/EmployeeServiceApplication.java
@@ -2,12 +2,13 @@ package com.hasitha.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
 	}
-
 }
